b9951b3ed8a21bad45db8dac0fa1bd7250156351
sm11t <adatta18@asu.edu>
2025-02-21 12:36:37 -0700

    parser works 90/90

--- Project1/provided_code/parser.cc
+++ Project1/provided_code/parser.cc
@@ -1,398 +1,372 @@
 /*
- * Copyright (C) Rida Bazzi, 2020
- *
- * Do not share this file with anyone
+ * Minimal direct parser that checks the grammar without storing anything.
+ * This style is based on your sample code that passes all 90 test cases.
  */
 
  #include "parser.h"
  #include <iostream>
  #include <cstdlib>
- #include <sstream>
  
  using namespace std;
  
  // ---------------------- Error Handling ----------------------
- void Parser::syntax_error() {
-     cout << "SYNTAX ERROR !!!!!&%!!\n";
+ void Parser::syntax_error()
+ {
+     cout << "SYNTAX ERROR !!!!!&%!!" << endl;
      exit(1);
  }
  
- Token Parser::expect(TokenType expected_type) {
-     Token t = lexer.GetToken();
-     if (t.token_type != expected_type)
+ Token Parser::expect(TokenType expected_type)
+ {
+     Token token = lexer.GetToken();
+     if (token.token_type != expected_type)
+     {
          syntax_error();
-     return t;
+     }
+     return token;
  }
  
  // ---------------------- Constructor -------------------------
  Parser::Parser() {
-     for (int i = 0; i < 7; i++)
-         tasks[i] = false;
+     // If you want to initialize anything, do it here.
  }
  
- bool Parser::isTaskSelected(int taskNumber) {
-     if (taskNumber >= 1 && taskNumber <= 6)
-         return tasks[taskNumber];
-     return false;
+ // ---------------------- ConsumeAllInput() -------------------
+ void Parser::ConsumeAllInput()
+ {
+     // Reads and prints any remaining tokens after input() is done.
+     Token token;
+     int i = 1;
+     token = lexer.peek(i);
+     token.Print();
+     while (token.token_type != END_OF_FILE)
+     {
+         i = i + 1;
+         token = lexer.peek(i);
+         token.Print();
+     }
+     token = lexer.GetToken();
+     token.Print();
+     while (token.token_type != END_OF_FILE)
+     {
+         token = lexer.GetToken();
+         token.Print();
+     }
  }
  
- // ---------------------- Overall Parsing ---------------------
- void Parser::ConsumeAllInput() {
-     parse_input();
- }
  
- void Parser::parse_input() {
-     parse_program();
+ void Parser::input()
+ {
+     program();
      expect(END_OF_FILE);
  }
  
- void Parser::parse_program() {
-     parse_tasks_section();
-     parse_poly_section();
-     parse_execute_section();
-     parse_inputs_section();
-     printFullInput();
+ void Parser::program()
+ {
+     tasks_section();
+     poly_section();
+     execute_section();
+     inputs_section();
  }
  
- // ---------------------- TASKS Section -----------------------
- void Parser::parse_tasks_section() {
+ // ---------------------- tasks_section -----------------------
+
+ void Parser::tasks_section()
+ {
      expect(TASKS);
-     parse_num_list(); // num_list → NUM | NUM num_list
+     num_list();
  }
  
- void Parser::parse_num_list() {
-     Token t = expect(NUM);
-     // For now, we simply consume NUM tokens.
-     if (lexer.peek(1).token_type == NUM)
-         parse_num_list();
+ void Parser::num_list()
+ {
+     expect(NUM);
+     while (lexer.peek(1).token_type == NUM)
+     {
+         expect(NUM);
+     }
  }
  
- // ---------------------- POLY Section ------------------------
- void Parser::parse_poly_section() {
+ // ---------------------- poly_section ------------------------
+
+ void Parser::poly_section()
+ {
      expect(POLY);
-     parse_poly_decl_list();
+     poly_decl_list();
  }
  
- void Parser::parse_poly_decl_list() {
-     parse_poly_decl();
-     Token t = lexer.peek(1);
-     if (t.token_type == ID)
-         parse_poly_decl_list();
+
+ void Parser::poly_decl_list()
+ {
+     poly_decl();
+     while (lexer.peek(1).token_type == ID)
+     {
+         poly_decl();
+     }
  }
- 
- void Parser::parse_poly_decl() {
-     PolyHeader header = parse_poly_header();
+
+ void Parser::poly_decl()
+ {
+     poly_header();
      expect(EQUAL);
-     PolyBody body = parse_poly_body();
+     poly_body();
      expect(SEMICOLON);
-     
-     PolyDecl decl;
-     decl.header = header;
-     decl.body = body;
-     polyDeclarations.push_back(decl);
  }
  
- PolyHeader Parser::parse_poly_header() {
-     PolyHeader header;
-     Token polyName = expect(ID);
-     header.name = polyName.lexeme;
-     if (lexer.peek(1).token_type == LPAREN) {
+
+ void Parser::poly_header()
+ {
+     poly_name();
+     if (lexer.peek(1).token_type == LPAREN)
+     {
          expect(LPAREN);
-         header.params = parse_id_list();
+         id_list();
          expect(RPAREN);
-     } else {
-         header.params.push_back("x"); // default parameter
      }
-     return header;
  }
  
- std::vector<std::string> Parser::parse_id_list() {
-     std::vector<std::string> params;
-     Token firstId = expect(ID);
-     params.push_back(firstId.lexeme);
-     while (lexer.peek(1).token_type == COMMA) {
+
+ void Parser::poly_name()
+ {
+     expect(ID);
+ }
+ 
+
+ void Parser::id_list()
+ {
+     expect(ID);
+     while (lexer.peek(1).token_type == COMMA)
+     {
          expect(COMMA);
-         Token nextId = expect(ID);
-         params.push_back(nextId.lexeme);
+         id_list();
      }
-     return params;
+ }
+
+ void Parser::poly_body()
+ {
+     term_list();
  }
  
- PolyBody Parser::parse_poly_body() {
-     PolyBody body;
-     // term_list → term | term add_operator term_list
-     Term t = parse_term();
-     body.terms.push_back(t);
-     while (lexer.peek(1).token_type == PLUS || lexer.peek(1).token_type == MINUS) {
-         Token op = lexer.GetToken(); // consume PLUS or MINUS
-         Term nextTerm = parse_term();
-         if (op.token_type == MINUS)
-             nextTerm.coefficient = -nextTerm.coefficient;
-         body.terms.push_back(nextTerm);
+
+ void Parser::term_list()
+ {
+     term();
+     while (lexer.peek(1).token_type == PLUS || lexer.peek(1).token_type == MINUS)
+     {
+         add_operator();
+         term();
      }
-     return body;
  }
  
- Term Parser::parse_term() {
-     Term term;
-     term.coefficient = 1;
-     // term → monomial_list | coefficient monomial_list | coefficient
-     if (lexer.peek(1).token_type == NUM) {
-         Token numToken = lexer.GetToken();
-         term.coefficient = std::stoi(numToken.lexeme);
-         if (lexer.peek(1).token_type == ID || lexer.peek(1).token_type == LPAREN)
-             term.monomials = parse_monomial_list();
-     } else {
-         term.monomials = parse_monomial_list();
-     }
-     return term;
+
+ void Parser::add_operator()
+ {
+     Token t = lexer.peek(1);
+     if (t.token_type == MINUS)
+         expect(MINUS);
+     else if (t.token_type == PLUS)
+         expect(PLUS);
+     else
+         syntax_error();
  }
  
- std::vector<Monomial> Parser::parse_monomial_list() {
-     std::vector<Monomial> list;
-     list.push_back(parse_monomial());
-     while (lexer.peek(1).token_type == ID || lexer.peek(1).token_type == LPAREN) {
-         list.push_back(parse_monomial());
+
+ void Parser::term()
+ {
+     Token t = lexer.peek(1);
+     if (t.token_type == NUM)
+     {
+         coefficient();
+         Token t1 = lexer.peek(1);
+         if (t1.token_type == ID || t1.token_type == LPAREN)
+         {
+             monomial_list();
+         }
      }
-     return list;
- }
- 
- Monomial Parser::parse_monomial() {
-     Monomial mono;
-     mono.exponent = 1;
-     mono.primary = parse_primary();
-     // monomial → primary | primary exponent
-     if (lexer.peek(1).token_type == POWER) {
-         expect(POWER);
-         Token numToken = expect(NUM);
-         mono.exponent = std::stoi(numToken.lexeme);
+     else if (t.token_type == ID || t.token_type == LPAREN)
+     {
+         monomial_list();
      }
-     return mono;
- }
- 
- Primary Parser::parse_primary() {
-     Primary prim;
-     // primary → ID | LPAREN term_list RPAREN
-     if (lexer.peek(1).token_type == ID) {
-         Token idToken = expect(ID);
-         prim.type = Primary::VAR;
-         prim.var = idToken.lexeme;
-         prim.group = nullptr;
-     } else if (lexer.peek(1).token_type == LPAREN) {
-         expect(LPAREN);
-         PolyBody innerBody = parse_poly_body();
-         expect(RPAREN);
-         prim.type = Primary::EXPR;
-         prim.group = new PolyBody(innerBody);
-     } else {
+     else
+     {
          syntax_error();
      }
-     return prim;
  }
  
- // ---------------------- EXECUTE Section ---------------------
- void Parser::parse_execute_section() {
+
+ void Parser::coefficient()
+ {
+     expect(NUM);
+ }
+
+ void Parser::monomial_list()
+ {
+     monomial();
+     while (lexer.peek(1).token_type == ID || lexer.peek(1).token_type == LPAREN)
+     {
+         monomial_list();
+     }
+ }
+ 
+
+ void Parser::monomial()
+ {
+     primary();
+     if (lexer.peek(1).token_type == POWER)
+     {
+         exponent();
+     }
+ }
+ 
+
+ void Parser::exponent()
+ {
+     expect(POWER);
+     expect(NUM);
+ }
+
+ void Parser::primary()
+ {
+     if (lexer.peek(1).token_type == ID)
+     {
+         expect(ID);
+     }
+     else if (lexer.peek(1).token_type == LPAREN)
+     {
+         expect(LPAREN);
+         term_list();
+         expect(RPAREN);
+     }
+     else
+     {
+         syntax_error();
+     }
+ }
+ 
+ // ---------------------- execute_section ----------------------
+
+ void Parser::execute_section()
+ {
      expect(EXECUTE);
-     parse_statement_list();
+     statement_list();
  }
  
- void Parser::parse_statement_list() {
-     // statement_list → statement | statement statement_list
-     while (true) {
-         Token t = lexer.peek(1);
-         if (t.token_type == INPUT)
-             parse_input_statement();
-         else if (t.token_type == OUTPUT)
-             parse_output_statement();
-         else if (t.token_type == ID)
-             parse_assign_statement();
-         else
-             break;
+
+ void Parser::statement_list()
+ {
+     statement();
+     while (lexer.peek(1).token_type == INPUT
+         || lexer.peek(1).token_type == OUTPUT
+         || lexer.peek(1).token_type == ID)
+     {
+         statement_list();
      }
  }
  
- void Parser::parse_input_statement() {
-     Statement stmt;
+
+ void Parser::statement()
+ {
+     Token nextToken = lexer.peek(1);
+     if (nextToken.token_type == INPUT)
+     {
+         input_statement();
+     }
+     else if (nextToken.token_type == OUTPUT)
+     {
+         output_statement();
+     }
+     else if (nextToken.token_type == ID)
+     {
+         assign_statement();
+     }
+     else
+     {
+         syntax_error();
+     }
+ }
+ 
+
+ void Parser::input_statement()
+ {
      expect(INPUT);
-     Token varToken = expect(ID);
+     expect(ID);
      expect(SEMICOLON);
-     stmt.stmt_type = INPUT_STMT;
-     stmt.var = varToken.lexeme;
-     statements.push_back(stmt);
  }
  
- void Parser::parse_output_statement() {
-     Statement stmt;
+
+ void Parser::output_statement()
+ {
      expect(OUTPUT);
-     Token varToken = expect(ID);
+     expect(ID);
      expect(SEMICOLON);
-     stmt.stmt_type = OUTPUT_STMT;
-     stmt.var = varToken.lexeme;
-     statements.push_back(stmt);
  }
  
- void Parser::parse_assign_statement() {
-     Statement stmt;
-     Token varToken = expect(ID);
+
+ void Parser::assign_statement()
+ {
+     expect(ID);
      expect(EQUAL);
-     PolyBody evalBody = parse_poly_evaluation();
+     poly_evaluation();
      expect(SEMICOLON);
-     stmt.stmt_type = ASSIGN_STMT;
-     stmt.var = varToken.lexeme;
-     stmt.poly_eval = evalBody;
-     statements.push_back(stmt);
  }
  
- PolyBody Parser::parse_poly_evaluation() {
-     // poly_evaluation → poly_name LPAREN argument_list RPAREN
-     // For our purposes, we'll parse this similarly to a poly_body expression.
-     PolyBody evalBody;
-     expect(ID); // poly_name; we don't store it here.
+
+ void Parser::poly_evaluation()
+ {
+     poly_name();
      expect(LPAREN);
-     evalBody = parse_poly_body();
+     argument_list();
      expect(RPAREN);
-     return evalBody;
  }
  
- void Parser::parse_argument_list() {
-     // Not used separately because we re-use poly evaluation parsing.
-     parse_argument();
-     while (lexer.peek(1).token_type == COMMA) {
+
+ void Parser::argument_list()
+ {
+     argument();
+     while (lexer.peek(1).token_type == COMMA)
+     {
          expect(COMMA);
-         parse_argument();
+         argument_list();
      }
  }
  
- void Parser::parse_argument() {
-     // argument → ID | NUM | poly_evaluation
-     Token t = lexer.peek(1);
-     if (t.token_type == ID) {
-         if (lexer.peek(2).token_type == LPAREN)
-             parse_poly_evaluation();
+
+ void Parser::argument()
+ {
+     Token nextToken = lexer.peek(1);
+     if (nextToken.token_type == ID)
+     {
+         Token t1 = lexer.peek(2);
+         if (t1.token_type == LPAREN)
+             poly_evaluation();
          else
              expect(ID);
-     } else if (t.token_type == NUM) {
+     }
+     else if (nextToken.token_type == NUM)
          expect(NUM);
-     } else {
-         parse_poly_evaluation();
-     }
+     else
+         syntax_error();
  }
  
- // ---------------------- INPUTS Section ---------------------
- void Parser::parse_inputs_section() {
+ // ---------------------- inputs_section -----------------------
+ // inputs_section → INPUTS num_list
+ void Parser::inputs_section()
+ {
      expect(INPUTS);
-     while (lexer.peek(1).token_type == NUM) {
-         Token t = expect(NUM);
-         inputs.push_back(std::stoi(t.lexeme));
-     }
- }
- 
- // ---------------------- Full Print Function ---------------------
- void Parser::printFullInput() {
-     cout << "Tasks: ";
-     for (int i = 1; i < 7; i++) {
-         if (tasks[i])
-             cout << i << " ";
-     }
-     cout << "\n\n";
-     
-     cout << "Polynomial Declarations:\n";
-     for (const auto &decl : polyDeclarations) {
-         cout << "Poly Name: " << decl.header.name << "\n";
-         cout << "Parameters: ";
-         for (const auto &param : decl.header.params)
-             cout << param << " ";
-         cout << "\nBody: " << polyBodyToString(decl.body) << "\n";
-         cout << "--------------------------\n";
-     }
-     cout << "\n";
-     
-
-     cout << "Execute Statements:\n";
-     for (const auto &stmt : statements) {
-         switch (stmt.stmt_type) {
-             case INPUT_STMT:
-                 cout << "INPUT " << stmt.var << ";\n";
-                 break;
-             case OUTPUT_STMT:
-                 cout << "OUTPUT " << stmt.var << ";\n";
-                 break;
-             case ASSIGN_STMT:
-                 cout << stmt.var << " = " << polyBodyToString(stmt.poly_eval) << ";\n";
-                 break;
-         }
-     }
-     cout << "\n";
-     
-
-     cout << "Inputs: ";
-     for (size_t i = 0; i < inputs.size(); i++) {
-         cout << inputs[i] << " ";
-     }
-     cout << "\n";
+     num_list();
  }
  
 
- void Parser::printPolyDeclarations() {
-     for (const auto &decl : polyDeclarations) {
-         cout << "Poly Name: " << decl.header.name << "\n";
-         cout << "Parameters: ";
-         for (const auto &param : decl.header.params)
-             cout << param << " ";
-         cout << "\nBody: " << polyBodyToString(decl.body) << "\n";
-         cout << "--------------------------\n";
-     }
- }
+ // Optional: function to "printFullInput" or do nothing
+//  void Parser::printFullInput()
+//  {
+//      cout << "Parsing finished successfully.\n";
+//  }
  
- // ---------------------- Helper Functions for Converting Expression to String ----------------------
- std::string primaryToString(const Primary& prim) {
-     if (prim.type == Primary::VAR)
-         return prim.var;
-     else {
-         std::string res = "(";
-         if (prim.group)
-             res += polyBodyToString(*(prim.group));
-         res += ")";
-         return res;
-     }
- }
- 
- std::string monomialToString(const Monomial& mono) {
-     std::ostringstream oss;
-     oss << primaryToString(mono.primary);
-     if (mono.exponent != 1)
-         oss << "^" << mono.exponent;
-     return oss.str();
- }
- 
- std::string termToString(const Term& term) {
-     std::ostringstream oss;
-     oss << term.coefficient;
-     for (const auto &mono : term.monomials)
-         oss << monomialToString(mono);
-     return oss.str();
- }
- 
- std::string polyBodyToString(const PolyBody& body) {
-     std::ostringstream oss;
-     if (!body.terms.empty()) {
-         oss << termToString(body.terms[0]);
-         for (size_t i = 1; i < body.terms.size(); i++) {
-             if (body.terms[i].coefficient >= 0)
-                 oss << " + " << termToString(body.terms[i]);
-             else {
-                 std::string termStr = termToString(body.terms[i]);
-                 oss << " - " << termStr.substr(1);
-             }
-         }
-     }
-     return oss.str();
- }
- 
- 
- int main() {
+ int main()
+ {
      Parser parser;
-     parser.parse_program();
+     parser.input();         
+     parser.ConsumeAllInput();  
      return 0;
  }
  
--- Project1/provided_code/parser.h
+++ Project1/provided_code/parser.h
@@ -1,128 +1,49 @@
-/*
- * Copyright (C) Rida Bazzi, 2019
- *
- * Do not share this file with anyone
- */
 #ifndef __PARSER_H__
 #define __PARSER_H__
 
 #include <string>
-#include <vector>
 #include "lexer.h"
 
-// ---------- Data Structures for POLY Section ----------
-
-// Represents a polynomial header (name and parameters)
-struct PolyHeader {
-    std::string name;                  // e.g. "F"
-    std::vector<std::string> params;   // e.g. {"x", "y"}
-};
-
-// Forward declaration for PolyBody (used in Primary)
-struct PolyBody;
-
-// Represents a primary expression
-struct Primary {
-    enum Type { VAR, EXPR } type;      // either a variable or a parenthesized expression
-    std::string var;                   // if type == VAR
-    PolyBody* group;                   // if type == EXPR (points to the inner expression)
-};
-
-// Represents a monomial: a primary possibly raised to an exponent
-struct Monomial {
-    Primary primary;
-    int exponent;                      // default is 1 if omitted
-};
-
-// Represents a term: an optional coefficient followed by a monomial list
-struct Term {
-    int coefficient;                   // default is 1 if not specified
-    std::vector<Monomial> monomials;
-};
-
-// Represents a polynomial body (i.e. a term list)
-struct PolyBody {
-    std::vector<Term> terms;
-};
-
-// Represents a complete polynomial declaration.
-struct PolyDecl {
-    PolyHeader header;
-    PolyBody body;
-};
-
-// ---------- Data Structures for EXECUTE Section ----------
-
-enum StmtType { INPUT_STMT, OUTPUT_STMT, ASSIGN_STMT };
-
-struct Statement {
-    StmtType stmt_type;
-    std::string var;       // For INPUT, OUTPUT, or the LHS of an assignment.
-    // For assignments, we store the parsed poly_evaluation as a PolyBody.
-    PolyBody poly_eval;
-};
-
-typedef std::vector<Statement> StmtList;
-
-// ----------------- Parser Class ---------------------
 class Parser {
   public:
-    Parser();
-    void ConsumeAllInput();
-    void parse_input();
-    void parse_program();
-    void parse_tasks_section();
-    void parse_poly_section();
-    void parse_execute_section();
-    void parse_inputs_section();
+    Parser();                     // Constructor
+    void ConsumeAllInput();       // Prints remaining tokens after parsing
+    void input();                 // Called from main(); calls program(), then expects EOF
 
-    // POLY Section functions
-    void parse_poly_decl_list();
-    void parse_poly_decl();
-    PolyHeader parse_poly_header();
-    std::vector<std::string> parse_id_list();
-    PolyBody parse_poly_body();
-
-    // Expression parsing functions (for polynomial bodies)
-    Term parse_term();
-    std::vector<Monomial> parse_monomial_list();
-    Monomial parse_monomial();
-    Primary parse_primary();
-
-    // EXECUTE Section functions
-    void parse_statement_list();
-    void parse_input_statement();
-    void parse_output_statement();
-    void parse_assign_statement();
-    PolyBody parse_poly_evaluation(); 
-    void parse_argument_list();
-    void parse_argument();
-
-    // INPUTS Section function
-    void parse_num_list();
-
-    // Debug printing functions
-    void printPolyDeclarations();
-    void printFullInput();
-
-    bool isTaskSelected(int taskNumber);
-    bool tasks[7];
-
-    // Storage for EXECUTE and INPUTS sections:
-    StmtList statements;
-    std::vector<int> inputs;
+    // Grammar productions
+    void program();
+    void tasks_section();
+    void num_list();
+    void poly_section();
+    void poly_decl_list();
+    void poly_decl();
+    void poly_header();
+    void id_list();
+    void poly_body();
+    void term_list();
+    void add_operator();
+    void term();
+    void coefficient();
+    void monomial_list();
+    void monomial();
+    void exponent();
+    void primary();
+    void execute_section();
+    void statement_list();
+    void statement();
+    void input_statement();
+    void output_statement();
+    void assign_statement();
+    void poly_name();
+    void poly_evaluation();
+    void argument_list();
+    void argument();
+    void inputs_section();
 
   private:
     LexicalAnalyzer lexer;
     void syntax_error();
     Token expect(TokenType expected_type);
-    std::vector<PolyDecl> polyDeclarations;
 };
-
-// ----------Helper functions----------
-std::string primaryToString(const Primary& prim);
-std::string monomialToString(const Monomial& mono);
-std::string termToString(const Term& term);
-std::string polyBodyToString(const PolyBody& body);
 
 #endif
